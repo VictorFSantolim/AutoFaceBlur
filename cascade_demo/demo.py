c41c56b913a066b64703bc2097819c6b08330c79
VictorFSantolim <victor@DESKTOP-BQEG7SP.localdomain>
2019-05-16 18:07:15 +0000

    Adicionado o contador de FPS no demo do cascade

--- cascade_demo/demo.py
+++ cascade_demo/demo.py
@@ -3,6 +3,7 @@
 #Importa as bibliotecas do openCV e do Numpy
 import cv2
 import numpy as np
+import time
 
 #Captura a imagem da webcam
 cap = cv2.VideoCapture(0)
@@ -13,6 +14,8 @@ profile_face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_profilefa
 cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
 
 while True:
+    #Conta o tempo de inicio do ciclo
+    tempo_inicio = time.time()
     #Le um frame da camera
     ret , frame = cap.read()
     #Transforma em escala de cinza (melhor para o reconheciento vai haar cascade)
@@ -31,13 +34,20 @@ while True:
          (x, y, w, h) = rect
          frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
+    #Conta o tempo de fim do ciclo
+    tempo_fim = time.time()
+    #Calcula FPS (frames por segundo)
+    fps = 1/(tempo_fim - tempo_inicio)
+
+    #Coloca o FPS na tela
+    cv2.putText(frame, str(fps) ,(10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,255),2,cv2.LINE_AA)
     #Mostra o frame com eventuais rostos detectados
     cv2.imshow("Frame", frame)
     #Se pressionar Esc ( = key 27), fecha a janela
     key = cv2.waitKey(1)
     if key == 27:
         break
-
+    
 #Libera a camera e fecha as janelas
 cap.release()
 cv2.destroyAllWindows()
