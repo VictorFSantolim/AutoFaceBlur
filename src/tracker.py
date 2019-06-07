68af0462a3881d1f6a0b5558508e5e682b5270fd
Rodrigo Caus <digocaus@hotmail.com>
2019-06-07 11:42:19 -0300

    Código formatado um pouquinho melhor e mais legível. Comentários em inglês. Não usa mais copy. Estratégia de decréscimo do _missing counter_ melhorada (uma face que é perdida com frequência tem maior penalidade de ser descartada do registro.

--- src/tracker.py
+++ src/tracker.py
@@ -1,8 +1,8 @@
 import math
-import copy
+
 
 class faceObject:
-    'Classe de base para rastrear faces'
+    'Class for a face tracking'
 
     history_size_max = 50
 
@@ -12,11 +12,11 @@ class faceObject:
         self.center = (int(x + w/2.0), int(y + h/2.0))
         self.radius = max(w, h)/2.0
         self.history.append(self.center)
-    
+
     def update(self, aface):
-        # Atualiza o centro para a nova face
+        # Updates de center based on a face center
         self.center = aface.center
-        # O novo raio eh a media do raio antigo e do raio da nova face
+        # New radius is the pounded average of old radius and a face radius
         keep_radius = 0.64
         greater_radius = max(self.radius, aface.radius)
         minor_radius = min(self.radius, aface.radius)
@@ -26,16 +26,18 @@ class faceObject:
             self.history.pop(0)
 
     def getrect(self):
-        # Retorna um quadrado que sobrescreve no plano o ciculo definido por center-radius
+        # Returns a square that overrides the (center, radius) circle of this face
         return (int(self.center[0]-self.radius), int(self.center[1]-self.radius), int(2.0*self.radius), int(2.0*self.radius))
 
     def getpredictcenter(self, n):
         if len(self.history) <= 1:
             return self.center
         else:
-            # Previsao linear de n frames futuros baseado no historico
-            diffx = (self.history[len(self.history)-1][0] - self.history[0][0])/(len(self.history)-1)
-            diffy = (self.history[len(self.history)-1][1] - self.history[0][1])/(len(self.history)-1)
+            # Linear prediction based on center history n frames ahead
+            diffx = (self.history[len(self.history)-1][0] -
+                     self.history[0][0])/(len(self.history)-1)
+            diffy = (self.history[len(self.history)-1][1] -
+                     self.history[0][1])/(len(self.history)-1)
             return (int(n*diffx + self.center[0]), int(n*diffy + self.center[1]))
 
     def getpredictrect(self, n):
@@ -43,7 +45,7 @@ class faceObject:
         return (int(pcenter[0]-self.radius), int(pcenter[1]-self.radius), int(2.0*self.radius), int(2.0*self.radius))
 
     def __eq__(self, aface):
-        # Uma face eh igual se estiver no raio de acao de self
+        # Consider a face equal to this face if the center is on this face radius
         (x, y) = self.center
         (xa, ya) = aface.center
         alpha_trig_distance = 1.0
@@ -51,46 +53,49 @@ class faceObject:
 
 
 class faceTracker:
-    'Classe de rastreamento de faces - baseado em faceObject'
+    'Face tracking class based on faceObject'
 
     missingMax = 50
 
     def __init__(self):
         self.faceDict = dict()
         self.faceMissingCounter = dict()
-        # Controla os IDs que ja existiram no rastreamento
+        # Represents all the ID's already tracked
         self.faceIDCounter = 0
 
     def update(self, frameFacesList):
-        # Cria um dicionario a partir da lista de faces do frame
+        # Creates a dictionary based on faces list of the frame
         frameFacesDict = dict(enumerate(frameFacesList))
         deleteKeyList = list()
-        # Associa cada face do registro a alguma(s) face(s) do frame
+        # Creates a relationship between faces on the registry
+        # and faces on the frame
         for faceID, regFace in self.faceDict.items():
-            equalFaces = [ key for key in frameFacesDict if regFace == frameFacesDict[key] ]
+            equalFaces = [key for key in frameFacesDict if regFace == frameFacesDict[key]]
             if len(equalFaces) == 0:
-                # A face eh considerada perdida caso nao tenha ninguem para associar
-                self.faceMissingCounter[faceID] = self.faceMissingCounter[faceID] + 1
+                # Consider missing face if no face on the frame id equal to this face
+                self.faceMissingCounter[faceID] += 1
                 if self.faceMissingCounter[faceID] > faceTracker.missingMax or self.faceMissingCounter[faceID] > 2*len(regFace.history):
                     deleteKeyList.append(faceID)
             else:
-                # Atualiza baseado no raio mais próximo
+                # Update based on the closest face on equalFaces list
                 closest = equalFaces[0]
                 for key in equalFaces:
                     if abs(self.faceDict[faceID].radius - frameFacesDict[key].radius) < abs(self.faceDict[faceID].radius - frameFacesDict[closest].radius):
                         closest = key
-                
+
                 trig_distance = 0.5
                 if frameFacesDict[closest].radius < trig_distance*regFace.radius:
-                    # Raio muito menor, possivel falso positivo
-                    self.faceMissingCounter[faceID] = self.faceMissingCounter[faceID] + 1
+                    # If closest face radius is too little, it does not update
+                    self.faceMissingCounter[faceID] += 1
                     if self.faceMissingCounter[faceID] > faceTracker.missingMax:
                         deleteKeyList.append(faceID)
                 else:
                     regFace.update(frameFacesDict[closest])
-                    self.faceMissingCounter[faceID] = 0
+                    self.faceMissingCounter[faceID] -= 10
+                    if self.faceMissingCounter[faceID] < 0:
+                        self.faceMissingCounter[faceID] = 0
 
-                # Remove todas as faces "iguais" do dicionario de faces do frame
+                # Remove all equalFaces on dictionary
                 for key in equalFaces:
                     del frameFacesDict[key]
 
@@ -98,25 +103,26 @@ class faceTracker:
             del self.faceDict[key]
             del self.faceMissingCounter[key]
 
-        # Para as faces do frame restantes, acrescentar no registro
+        # The left faces are new on the registry
         for newFaceID in frameFacesDict:
-            self.faceDict[self.faceIDCounter] = copy.deepcopy(frameFacesDict[newFaceID])
+            self.faceDict[self.faceIDCounter] = faceObject(
+                frameFacesDict[newFaceID].getrect())
             self.faceMissingCounter[self.faceIDCounter] = 0
-            self.faceIDCounter = self.faceIDCounter + 1
+            self.faceIDCounter += 1
 
         frameFacesDict.clear()
         del frameFacesDict
-    
-    def getFaces(self, framerate = 30.0):
-        # Cria uma lista de retangulos que representam as faces
+
+    def getFaces(self, framerate=30.0):
+        # Create a list of squares around the faces
         faceList = list()
         for faceID in self.faceDict:
             if self.faceMissingCounter[faceID] > 0:
                 framerate_div = framerate/30.0
-                
+
                 self.faceDict[faceID].radius = (1.005)*self.faceDict[faceID].radius
-                faceList.append(self.faceDict[faceID].getpredictrect(self.faceMissingCounter[faceID]/framerate_div))
+                faceList.append(self.faceDict[faceID].getpredictrect(
+                    self.faceMissingCounter[faceID]/framerate_div))
             else:
                 faceList.append(self.faceDict[faceID].getrect())
         return faceList
-
